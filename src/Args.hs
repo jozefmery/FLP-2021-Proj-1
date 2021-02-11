aa3b8276122606a722ec492368ac030a7aaf3bea
Dodmeister <jozefdmery@gmail.com>
2021-02-11 18:00:30 +0100

    added comments, minor refactors

--- src/Args.hs
+++ src/Args.hs
@@ -3,7 +3,7 @@
     Author:   Jozef Méry - xmeryj00@vutbr.cz
     Project:  FLP-2021-xmeryj00-simplify-bkg
     Date:     9.2.2021
-    Description: TODO  
+    Description: Command-line argument parsing into program options.  
 -}
 
 module Src.Args where
@@ -17,50 +17,54 @@ import System.Environment( getArgs )
 
 --- imports ---
 
--- TODO
+-- Program options structure representing command-line
+-- argument options. 
 data Options = Options 
-  { help :: Bool
-  , internal :: Bool
-  , step1 :: Bool
-  , step2 :: Bool 
-  , input :: Maybe FilePath
+  { help      :: Bool
+  , internal  :: Bool
+  , step1     :: Bool
+  , step2     :: Bool 
+  , input     :: Maybe FilePath
   } deriving Show
 
--- TODO
+-- Define default options.
+-- Default options are invalid (missing a stage).
 defaultOptions :: Options
 defaultOptions = Options
-  { help = False
-  , internal = False
-  , step1 = False
-  , step2 = False
-  , input = Nothing 
+  { help      = False
+  , internal  = False
+  , step1     = False
+  , step2     = False
+  , input     = Nothing 
   }
 
--- TODO
+-- Program usage string.
 usage :: String
 usage = "Usage: simplify-bkg {-h | -i | -1 | -2} [<input_file>]"
 
--- TODO
+-- GetOpt Options transformer definitions.
 optionsTransformer :: [OptDescr (Options -> Options)]
 optionsTransformer = 
-  [ Option ['h'] ["help"] (NoArg (\ opts -> opts { help = True })) "h"
-  , Option ['i'] [] (NoArg (\ opts -> opts { internal = True })) "TODO i"
-  , Option ['1'] [] (NoArg (\ opts -> opts { step1 = True })) "1"
-  , Option ['2'] [] (NoArg (\ opts -> opts { step2 = True })) "2"
+  [ Option ['h'] ["help"] (NoArg (\ opts -> opts { help = True })) "Print this help message."
+  , Option ['i'] [] (NoArg (\ opts -> opts { internal = True })) "Print the internal representation of the parsed input grammar."
+  , Option ['1'] [] (NoArg (\ opts -> opts { step1 = True })) "Print the grammar after the first step. "
+  , Option ['2'] [] (NoArg (\ opts -> opts { step2 = True })) "Print the grammar after the second step."
   ]
 
-options' :: [String] -> IO (Options, [String])
-options' argv =
+-- Runs getOpt and returns transformed options and
+-- a list of arguments.
+options' :: IO (Options, [String])
+options' = do
+  argv <- getArgs
   case getOpt Permute optionsTransformer argv of
     (o, n, []) -> return (foldl (flip id) defaultOptions o, n)
     (_, _, errs) -> ioError (userError (concat errs ++ usageInfo usage optionsTransformer))
-    
+
+-- Fills in the input field of the Options structure if any.
+-- If multiple are provided, the first is chosen.
 options :: IO Options
 options = do
-  argv <- getArgs
-  (flags, args) <- options' argv
-  return $ setInputPath flags args
-
-setInputPath :: Options -> [String] -> Options
-setInputPath opts [] = opts 
-setInputPath opts (first:_) = opts{ input = Just first }
+  (opts, args) <- options'
+  case args of 
+    [] -> return opts
+    first:_ -> return opts{ input = Just first }
