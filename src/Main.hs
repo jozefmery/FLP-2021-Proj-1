ce2e1877350ba8427dbff7ab3712f7080b7558d8
Dodmeister <jozefdmery@gmail.com>
2021-02-20 14:34:33 +0100

    refactored main with placeholder run

--- src/Main.hs
+++ src/Main.hs
@@ -1,52 +1,79 @@
 {-
-    File:     Main.hs
-    Author:   Jozef Méry - xmeryj00@vutbr.cz
-    Project:  FLP-2021-xmeryj00-simplify-bkg
-    Date:     9.2.2021
-    Description: TODO  
+  File:     Main.hs
+  Author:   Jozef Méry - xmeryj00@vutbr.cz
+  Project:  FLP-2021-xmeryj00-simplify-bkg
+  Date:     9.2.2021
+  Description: Program entry point. Loads command-line arguments, executes requested
+               actions, prints results, and handles errors.  
 -}
+
+--- extensions ---
+{-# LANGUAGE NamedFieldPuns #-}
+--- extensions ---
 
 module Main where
 
 --- imports ---
+import Args
+  ( Options(..)
+  , fullUsage 
+  , options
+  )
 
--- GetOpt code adopted from official docs
--- https://hackage.haskell.org/package/base-4.14.1.0/docs/System-Console-GetOpt.html
-import System.Console.GetOpt
-import System.Environment
+import System.Exit
+  ( exitSuccess
+  , exitFailure
+  )
 
-import Src.Args
+import Result( Result(..) )
+
+import Simplify -- TODO
 
 --- imports ---
 
-run :: Options -> IO()
-run Options{ help = True } = putStrLn $ usageInfo usage optionsTransformer
+printError :: String -> IO()
+printError err = putStrLn $ "Error: " ++ err
 
-run Options { internal = True
-            , step1 = False
-            , step2 = False
-            } = 
-              print "internal" 
+run :: Result Options -> IO()
+run (Err err) = do
+  printError err
+  exitFailure
 
-run Options { internal = False
-            , step1 = True
-            , step2 = False
-            } = 
-              print "step1" 
+run (Ok Options{ help = True }) = do
+  putStrLn fullUsage
+  exitSuccess
 
-run Options { internal = False
-            , step1 = False
-            , step2 = True
-            } = 
-              print "step2"
+run (Ok Options
+          { internal  = True
+          , step1     = False
+          , step2     = False
+          , input     
+          }) = do
+  loadGrammar input -- TODO
+  exitSuccess
 
-run Options{} = do
-  putStrLn "Invalid parameters"
-  putStrLn usage
+run (Ok Options 
+          { internal  = False
+          , step1     = True
+          , step2     = False
+          }) = do
+  print "step1" -- TODO
+  exitSuccess
 
-main :: IO ()
--- main = 
-main = do
-  opts <- options
-  print opts
-  run opts
+run (Ok Options
+          { internal  = False
+          , step1     = False
+          , step2     = True
+          }) = do
+  print "step2" -- TODO
+  exitSuccess
+
+-- handle invalid flag combination (multiple flags or none)
+run (Ok Options{}) = do
+  printError "Invalid flag combination (use exactly one)"
+  putStrLn fullUsage
+  exitFailure
+
+main :: IO()
+-- get options from command-line arguments and run the program
+main = options >>= run
